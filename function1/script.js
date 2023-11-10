81f1f32a240381693b658e5f759992b36c8c714c
Ceyda Candan <ekenmelih@gmail.com>
2023-11-10 17:38:18 +0300

    files via upload
    javascript functions

---
+++ function1/script.js
@@ -0,0 +1,49 @@
+
+/*
+//Function declaration
+
+function calcAge1(birthYear) {
+    return 2037-birthYear;
+}
+const age1= calcAge1(1991);
+
+//Function expression
+
+const calcAge2=function (birthYear) {
+    return 2037-birthYear;
+}
+const age2= calcAge2(1991);
+
+console.log(age1, age2)
+*/
+
+/* function totalNumber(number1, number2) {
+    return number1 + number2;
+
+}
+const plusNum = totalNumber(95,27);
+function extNumber(number1, number2) {
+    return number1 - number2;
+
+}
+const extNum = extNumber(95,27);
+
+
+
+
+console.log(plusNum,extNum);
+
+ */
+
+const totalNumber = function (number1,number2) {
+    return number1 + number2;
+}
+const plusNum = totalNumber(57,92)
+
+const extNumber = function (number1,number2) {
+    return number1 - number2;
+}
+const extNum = extNumber(157,92);
+
+console.log(plusNum,extNum);
+
